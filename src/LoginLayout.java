5f55f0e45034f103a562515d401e376374217a93
iseungho <ghehf51@naver.com>
2022-12-03 19:13:55 +0900

    Add : LoginLayout

---
+++ src/LoginLayout.java
@@ -0,0 +1,24 @@
+import javax.swing.*;
+
+public class LoginLayout extends JFrame{
+    private JTextField IdTextField;
+    private JPasswordField passwordField;
+    private JButton LogInButton;
+    private JButton SignUpButton;
+    private JPanel LoginPanel;
+    private JRadioButton AutoLoginRadio;
+
+    public LoginLayout(){
+        setContentPane(LoginPanel);
+        setSize(400, 600);
+        setTitle("홍톡2.0");
+        setResizable(false);
+        setVisible(true);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    public static void main(String[] args) {
+        new LoginLayout();
+    }
+
+}
